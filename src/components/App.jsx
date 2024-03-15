5708ec96fdd0a625577425705e8f6d6d4e3ab2d1
KorotinI <142391798+KorotinI@users.noreply.github.com>
2024-03-15 19:45:25 +0100

    fixLocalstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from "react";
 import Description from "./Description/Description";
 import "./App.css";
 import Options from "./Options/Options";
 import Feedback from "./Feedback/Feedback";
-import { useState } from "react";
 import Notification from "./Notification/Notification";
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+ 
+  const initialFeedback = JSON.parse(localStorage.getItem("feedback")) || {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  const [feedback, setFeedback] = useState(initialFeedback);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
   const updateFeedback = (feedbackType) => {
-    setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
+    }));
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
@@ -20,6 +35,7 @@ const App = () => {
   const resetFeedback = () => {
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
+
   return (
     <>
       <Description />
@@ -28,14 +44,15 @@ const App = () => {
         resetFeedback={resetFeedback}
         totalFeedback={totalFeedback}
       />
-      {totalFeedback !== 0 && (
+      {totalFeedback !== 0 ? (
         <Feedback
           feedback={feedback}
           totalFeedback={totalFeedback}
           positiveFeedback={positiveFeedback}
         />
+      ) : (
+        <Notification />
       )}
-      {totalFeedback === 0 && <Notification />}
     </>
   );
 };
